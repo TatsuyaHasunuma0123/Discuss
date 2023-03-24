8f9ee5a04cab0a02550f3b34073718040c4718cc
TatsuyaHasunuma0123 <tatsuya_0123@icloud.com>
2023-03-25 02:46:12 +0900

    add:  add comment to database

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,20 +1,31 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
+  alias DiscussWeb.{Topic, Comment}
+  alias Discuss.Repo
 
   @impl true
-  def join(name, _params, socket) do
-    IO.puts("---------------------")
-    IO.puts (name)
-    {:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
   @impl true
-  def handle_in(name, message, socket) do
-    IO.puts("----------")
-    IO.puts(name)
-    IO.inspect(message)
-    {:reply, :ok, socket}
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset = topic
+      |> Ecto.build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:reply, :ok, comment} ->
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
